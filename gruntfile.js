89af0823e5ffb9b6512cd956b24fdc389d6aae7c
Steven Black <steveb@stevenblack.com>
2014-07-03 01:24:03 -0400

    First-cut `gruntfile.js`and `package.json` files.

---
+++ gruntfile.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+
+module.exports = function( grunt ) {
+	"use strict";
+
+	// Project configuration.
+	grunt.initConfig( {
+
+		// Metadata.
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+	});
+
+};
