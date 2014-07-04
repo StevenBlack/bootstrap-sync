a3fb84becbc04773e8cb984fe494f014bbf1fa3f
Steven Black <steveb@stevenblack.com>
2014-07-04 00:49:34 -0400

    Grunt task for fetching fresh bootstrap source.

--- gruntfile.js
+++ gruntfile.js
@@ -4,11 +4,26 @@ module.exports = function( grunt ) {
 	"use strict";
 
 	// Project configuration.
-	grunt.initConfig( {
+	grunt.initConfig({
 
 		// Metadata.
 		pkg: grunt.file.readJSON( 'package.json' ),
 
+		copy: {
+			"bootstrap-source": {
+				nonull: true,
+				expand: true,
+				cwd: '<%= pkg.config.location.bootstrap %>/',
+				src: [ 'fonts/*', 'js/*', 'less/*' ],
+				dest: 'bootstrap/',
+			}
+		}
+
 	});
 
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+
+	grunt.registerTask( 'fetch-fresh', [ 'copy:bootstrap-source' ] );
+
 };
+
