535680f7db326cea4efebf5a4359653581b35e07
Steven Black <steveb@stevenblack.com>
2014-07-04 13:21:41 -0400

    Making use of the package.json config items, and also introducing less compile.

--- gruntfile.js
+++ gruntfile.js
@@ -15,13 +15,23 @@ module.exports = function( grunt ) {
 			'bootstrap-source': {
 				nonull: true,
 				expand: true,
-				cwd: '<%= pkg.config.location.bootstrap %>/',
-				src: [ 'fonts/*', 'js/*', 'less/*' ],
+				cwd: '<%= pkg.config.location.bootstrap.authoritative %>/',
+				src: [ 'fonts/*', 'js/*', 'less/**' ],
 				dest: 'bootstrap/',
 			},
 			'bootstrap-tweaks': {
 				src: [ 'less/*' ],
-				dest: 'bootstrap/',
+				dest: '<%= pkg.config.location.bootstrap.local %>/',
+			}
+		},
+
+		less: {
+			compileCore: {
+				options: {
+					strictMath: true,
+					outputSourceFiles: true
+				},
+				files: { '<%= pkg.config.location.deploy.css %>/<%= pkg.config.filename.css %>': '<%= pkg.config.location.bootstrap.local %>/less/custom.less' }
 			}
 		}
 
@@ -29,11 +39,17 @@ module.exports = function( grunt ) {
 
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
+
 
 	// Bootstrap tasks
 	grunt.registerTask( 'clean-bootstrap', [ 'clean:bootstrap-source' ] );
 	grunt.registerTask( 'fetch-fresh-bootstrap', [ 'copy:bootstrap-source' ] );
 	grunt.registerTask( 'apply-bootstrap-tweaks', [ 'copy:bootstrap-tweaks' ] );
 	grunt.registerTask( 'bootstrap', [ 'clean-bootstrap', 'fetch-fresh-bootstrap', 'copy:bootstrap-tweaks' ] );
+
+	// Less tasks
+	grunt.registerTask('less-compile', ['less:compileCore']);
+
 };
 
